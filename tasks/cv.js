cb46b170bcb1192173ba5141869e10b7b270d0fe
Marko Kercmar <m.kercmar@whow.net>
2015-03-29 16:59:36 +0200

    fixes and stuff..

--- tasks/cv.js
+++ tasks/cv.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('cv', 'Appends __cv hash on all IMG URLs in CSS', function() {
     var _fs = require('fs');
     var _crypto = require('crypto');
+    var _uri = require('uri-js');
 
     var _md5 = function(str) {
         return _crypto
@@ -26,6 +27,7 @@ module.exports = function(grunt) {
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
+        docroot: './',
     });
 
     // Iterate over all specified file groups.
@@ -39,24 +41,31 @@ module.exports = function(grunt) {
 
             var source = grunt.file.read(filepath);
 		
-            var pattern = /url\(.*?(gif|png|jpg|jpeg)\'\)/ig
+            var pattern = /url\(.*?(gif|png|jpg|jpeg)['"]{0,1}\)/ig
 
             var matches = source.match(pattern);
 
             for (var i in matches) {
                 var match = matches[i];
 
-                grunt.log.writeln(match);
+                if (match.indexOf('?') === 1) continue;
 
-                var clean = match.replace('../../', '/');
-                var filepath = clean.replace('url(', '').replace(')', '').replace("'", '').replace("'", '').replace('"', '').replace('"', '');
-                var filepath_fs = "./webroot/" + filepath;
+                var clean = match.replace('url(', '').replace(')', '').replace("'", '').replace("'", '').replace('"', '').replace('"', '');
+                var filepath = _uri.normalize(clean);
+                var filepath_fs = options.docroot + filepath;
+
+                if (filepath.charAt(0) != '/') filepath = '/' + filepath;
 
                 if (_fs.existsSync(filepath_fs)) {
                     var data = _fs.readFileSync(filepath_fs);
                     var hash = _md5(data);
 
                     source = source.replace(match, "url('" + filepath + "?__cv=" + hash + "')");
+
+                    //console.log(match);
+                    //console.log("url('" + filepath + "?__cv=" + hash + "')");
+                } else {
+                    //console.log(match);
                 }
             }
 
