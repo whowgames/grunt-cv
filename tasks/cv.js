f60d275ec1a5a9f3db78f12abb4459be53b80b9d
Marko Kercmar <m.kercmar@whow.net>
2016-08-18 17:18:11 +0200

    clean out existing __cv tags before putting new ones in..

--- tasks/cv.js
+++ tasks/cv.js
@@ -40,15 +40,24 @@ module.exports = function(grunt) {
             }
 
             var source = grunt.file.read(filepath);
-		
+
             var pattern = /url\(.*?['"]{0,1}\)/ig
 
             var matches = source.match(pattern);
 
             for (var i in matches) {
                 var match = matches[i];
+                var orig = match;
 
-                if (match.indexOf('?') === 1) continue;
+                //grunt.log.writeln(match);
+
+                if (orig.match(/\?__cv=[a-z0-9]{32}/ig) && orig.indexOf('&') < 0) {
+                    match = orig.substring(0, orig.indexOf('?')) + orig.substring(orig.indexOf('?')+38);
+                }
+
+                if (match.indexOf('?') === 1) {
+                    continue;
+                }
 
                 var clean = match.replace('url(', '').replace(')', '').replace("'", '').replace("'", '').replace('"', '').replace('"', '');
                 var filepath = _uri.normalize(clean);
@@ -61,17 +70,17 @@ module.exports = function(grunt) {
                     var hash = _md5(data);
                     hash = hash.substr(0, 30) + "00";
 
-                    source = source.replace(match, "url('" + filepath + "?__cv=" + hash + "')");
+                    source = source.replace(orig, "url('" + filepath + "?__cv=" + hash + "')");
 
-                    //console.log(match);
-                    //console.log("url('" + filepath + "?__cv=" + hash + "')");
+                    //grunt.log.writeln(orig);
+                    //grunt.log.writeln("url('" + filepath + "?__cv=" + hash + "')");
                 } else {
-                    //console.log(match);
+                    //grunt.log.writeln(match);
                 }
             }
 
             grunt.file.write(f.dest, source);
-        
+
             grunt.log.writeln('File "' + f.dest + '" written.');
         });
     });
