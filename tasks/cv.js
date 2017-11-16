49e47ca57ae6bf0c231141fae4704d649ee58883
Matthias Brock <m.brock@whow.net>
2017-11-16 10:06:10 +0100

    Update cv.js

--- tasks/cv.js
+++ tasks/cv.js
@@ -8,81 +8,78 @@
 
 'use strict';
 
+var _fs = require('fs');
+var _crypto = require('crypto');
+var _uri = require('uri-js');
+
+var _md5 = function(str) {
+    return _crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex');
+};
+
 module.exports = function(grunt) {
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    grunt.registerMultiTask('cv', 'Appends __cv hash on all IMG URLs in CSS', function() {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            docroot: './',
+        });
 
-  grunt.registerMultiTask('cv', 'Appends __cv hash on all IMG URLs in CSS', function() {
-    var _fs = require('fs');
-    var _crypto = require('crypto');
-    var _uri = require('uri-js');
-
-    var _md5 = function(str) {
-        return _crypto
-            .createHash('md5')
-            .update(str)
-            .digest('hex');
-    };
-
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-        docroot: './',
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-        // Concat specified files.
-        var src = f.src.filter(function(filepath) {
-            // Warn on and remove invalid source files (if nonull was set).
-            if (!grunt.file.exists(filepath)) {
-                return false;
-            }
-
-            var source = grunt.file.read(filepath);
-
-            var pattern = /url\(.*?['"]{0,1}\)/ig
-
-            var matches = source.match(pattern);
-
-            for (var i in matches) {
-                var match = matches[i];
-                var orig = match;
-
-                //grunt.log.writeln(match);
-
-                if (orig.match(/\?__cv=[a-z0-9]{32}/ig) && orig.indexOf('&') < 0) {
-                    match = orig.substring(0, orig.indexOf('?')) + orig.substring(orig.indexOf('?')+38);
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
+            // Concat specified files.
+            var src = f.src.filter(function(filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    return false;
                 }
 
-                if (match.indexOf('?') === 1) {
-                    continue;
+                var source = grunt.file.read(filepath);
+
+                var pattern = /url\(.*?['"]{0,1}\)/ig
+
+                var matches = source.match(pattern);
+
+                for (var i in matches) {
+                    var match = matches[i];
+                    var orig = match;
+
+                    source = source.replace(orig, "url('" + module.exports.getCV(match, options.docroot) + "')");
                 }
 
-                var clean = match.replace('url(', '').replace(')', '').replace("'", '').replace("'", '').replace('"', '').replace('"', '');
-                var filepath = _uri.normalize(clean);
-                var filepath_fs = options.docroot + filepath;
+                grunt.file.write(f.dest, source);
 
-                if (filepath.charAt(0) != '/') filepath = '/' + filepath;
-
-                if (_fs.existsSync(filepath_fs)) {
-                    var data = _fs.readFileSync(filepath_fs);
-                    var hash = _md5(data);
-                    hash = hash.substr(0, 30) + "00";
-
-                    source = source.replace(orig, "url('" + filepath + "?__cv=" + hash + "')");
-
-                    //grunt.log.writeln(orig);
-                    //grunt.log.writeln("url('" + filepath + "?__cv=" + hash + "')");
-                } else {
-                    //grunt.log.writeln(match);
-                }
-            }
-
-            grunt.file.write(f.dest, source);
-
-            grunt.log.writeln('File "' + f.dest + '" written.');
+                grunt.log.writeln('File "' + f.dest + '" written.');
+            });
         });
     });
-  });
 };
+module.exports.getCV = function(sourceFilePath, docroot) {
+    if (sourceFilePath.match(/\?__cv=[a-z0-9]{32}/ig) && sourceFilePath.indexOf('&') < 0) {
+        sourceFilePath = sourceFilePath.substring(0, sourceFilePath.indexOf('?')) + sourceFilePath.substring(sourceFilePath.indexOf('?')+38);
+    }
+
+    if (sourceFilePath.indexOf('?') === 1) {
+        return sourceFilePath;
+    }
+
+    var clean = sourceFilePath.replace('url(', '').replace(')', '').replace("'", '').replace("'", '').replace('"', '').replace('"', '');
+    var filepath = _uri.normalize(clean);
+    var filepath_fs = (docroot || "") + filepath;
+
+    if (filepath.charAt(0) != '/') filepath = '/' + filepath;
+
+    if (_fs.existsSync(filepath_fs)) {
+        var data = _fs.readFileSync(filepath_fs);
+        var hash = _md5(data);
+        hash = hash.substr(0, 30) + "00";
+
+        return filepath + "?__cv=" + hash;
+    }
+
+    return sourceFilePath;
+}
